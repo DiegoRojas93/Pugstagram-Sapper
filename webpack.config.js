90b1281af4940f9d79c16987a24a3ffabde46b99
Diego Rojas <diegorojas431@gmail.com>
2020-07-06 14:17:40 -0500

    Layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const webpack = require('webpack');
 const path = require('path');
-const config =  require('sapper/config/webpack,js');
+const config =  require('sapper/config/webpack.js');
 const pkg = require('./package.json');
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
-const alias = { svelte: path.resolve('node_modules', 'svelte') }
+const alias = { svelte: path.resolve('node_modules', 'svelte') };
 const extensions = ['.mjs', '.js', '.json', '.svelte', 'html'];
 const mainFields = ['svelte', 'module', 'browser', 'main'];
 
@@ -14,12 +14,12 @@ module.exports={
   client: {
     entry: config.client.entry(),
     output: config.client.output(),
-    resolve: { alias, extensions, mainFields},
+    resolve: { alias, extensions, mainFields },
     module: {
       rules: [
         {
           test: /\.(svelte|html)$/,
-          use:{
+          use: {
             loader: 'svelte-loader',
             options: {
               dev,
@@ -34,7 +34,7 @@ module.exports={
     plugins:[
       new webpack.DefinePlugin({
         'process.browser': true,
-        'process.env.NODE_EN': JSON.stringify(mode)
+        'process.env.NODE_ENV': JSON.stringify(mode)
       }),
     ].filter(Boolean),
     devtool: dev && 'inline-source-map'
@@ -44,9 +44,9 @@ module.exports={
     entry: config.server.entry(),
     output: config.server.output(),
     target: 'node',
-    resolve: {alias, extensions, mainFields},
-    externals: Object.keys(pkd.dependencies).concat('encoding'),
-    mode: {
+    resolve: { alias, extensions, mainFields },
+    externals: Object.keys(pkg.dependencies).concat('encoding'),
+    module: {
       rules: [
         {
           test: /\.(svelte|html)$/,
